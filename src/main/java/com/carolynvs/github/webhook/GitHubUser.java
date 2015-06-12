070ccdc8c1e7925b19c443ebdbbafb99622337ea
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-06-12 14:13:50 -0500

    Ignore fields that we don't care about when deserializing

--- src/main/java/com/carolynvs/github/webhook/GitHubUser.java
+++ src/main/java/com/carolynvs/github/webhook/GitHubUser.java
@@ -1,7 +1,9 @@
 package com.carolynvs.github.webhook;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GitHubUser
 {
     @JsonProperty("login")
